87f719018ad866ff5e9356f161f8ae91691e6a37
tyan <tyan@linkedin.com>
2016-11-13 22:35:34 -0800

    Add routes and all components' files

---
+++ src/routes.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Route, IndexRoute} from 'react-router';
+
+import App from './components/app';
+import DatasetList from './components/dataset_list';
+import DatasetNew from './components/dataset_new';
+import DatasetShow from './components/dataset_show';
+
+export default (
+  <Route path="/" component={App}>
+    <IndexRoute component={DatasetList} />
+    <Route path="dataset/new" component={DatasetNew} />
+    <Route path="dataset/:id" component={DatasetShow} />
+  </Route>
+);
