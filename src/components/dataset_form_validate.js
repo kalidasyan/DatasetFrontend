a0d8368209de7fbd81d99ae33c7d9bc68be3f298
tyan <tyan@linkedin.com>
2016-11-26 12:16:27 -0800

    Add Statistics Section - validate

--- src/components/dataset_form_validate.js
+++ src/components/dataset_form_validate.js
@@ -17,9 +17,32 @@ const validate = values => {
     errors.notificationList = 'Required'
   }
 */
+
+/*
+  if (!values.statistics || !values.statistics.length) {
+    errors.statistics = { _error: 'At least one statistic must be entered' }
+  } else {
+*/
+  if(values.statistics && values.statistics.length) {
+    const statisticsErrors = []
+    values.statistics.forEach((statistic, statisticIndex) => {
+      const statisticErrors = {}
+      if (!statistic || !statistic.id) {
+        statisticErrors.id = 'Must select a statistic';
+        statisticsErrors[statisticIndex] = statisticErrors
+      }
+    })
+    if(statisticsErrors.length) {
+      errors.statistics = statisticsErrors
+    }
+  }
+
+/*
   if (!values.datasetRules || !values.datasetRules.length) {
     errors.datasetRules = { _error: 'At least one dataset rule must be entered' }
   } else {
+*/
+  if (values.datasetRules && values.datasetRules.length) {
     const datasetRulesErrors = []
     values.datasetRules.forEach((rule, ruleIndex) => {
       const ruleErrors = {}
@@ -44,6 +67,7 @@ const validate = values => {
       errors.datasetRules = datasetRulesErrors
     }
   }
+
   return errors
 }
 
