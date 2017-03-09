5faace108f01e095de186a295c90fcf17bd69865
tyan <tyan@linkedin.com>
2017-03-08 16:10:28 -0800

    Display UI: show first highchart

--- src/components/statistic_graph.js
+++ src/components/statistic_graph.js
@@ -1,9 +1,25 @@
 import React, {Component} from 'react';
+import ReactHighcharts from 'react-highcharts';
+
+var config = {
+  title: {
+    text: "Test Title"
+  },
+  xAxis: {
+    categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+  },
+  series: [{
+    type: "column",
+    data: [29.9, 71.5, -106.4, 129.2, 144.0, -176.0, 135.6, 148.5, 216.4, 194.1, 295.6, 454.4]
+  }]
+};
 
 class StatisticGraph extends Component {
   render() {
     return (
-      <div>Statistic Graph!!</div>
+      <div>Statistic Graph!!
+        <ReactHighcharts config={config} />
+      </div>
     );
   }
 }
